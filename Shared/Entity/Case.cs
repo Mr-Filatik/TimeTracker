8654d54bd3691051423d00dca87afd05e7cc552f
Vladislav Filatov <v.filatov@softchamber.ru>
2023-10-07 17:33:10 +0400

    Create main entities
    Create main entities and create project folder structure.

---
+++ Shared/Entity/Case.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Shared.Entity
+{
+    public class Case
+    {
+        public string Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = null;
+        public Time Time { get; set; } = new Time();
+    }
+}
---
+++ Shared/Entity/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Shared.Entity
+{
+    public class Time
+    {
+        public int Hour { get; set; } = default;
+        public int Minute { get; set; } = default;
+        public int Second { get; set; } = default;
+    }
+}
