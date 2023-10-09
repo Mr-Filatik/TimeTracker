2434e0209484e69aafd6ae066b112ea2c9165a87
Vladislav Filatov <v.filatov@softchamber.ru>
2023-10-09 19:51:58 +0400

    Create Blasor project with dropdown

--- Shared/Entity/Time.cs
+++ Shared/Entity/Time.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,74 @@ namespace TimeTracker.Shared.Entity
 {
     public class Time
     {
-        public int Hour { get; set; } = default;
-        public int Minute { get; set; } = default;
-        public int Second { get; set; } = default;
+        #region Private Fields
+
+        private byte _hour = default;
+        private byte _minute = default;
+        private byte _second = default;
+
+        #endregion
+
+        #region Static Fields
+
+        public static byte HoursInDay = 24;
+        public static byte MinutesInHour = 60;
+        public static byte SecondsInMinute = 60;
+
+        #endregion
+
+        #region Properties
+
+        public byte Hour
+        {
+            get
+            {
+                return _hour;
+            }
+            set
+            {
+                if (value > (HoursInDay - 1))
+                {
+                    value -= HoursInDay;
+                }
+                _hour = value;
+            }
+        }
+
+        public byte Minute
+        {
+            get
+            {
+                return _minute;
+            }
+            set
+            {
+                if (value > (MinutesInHour - 1))
+                {
+                    value -= MinutesInHour;
+                    Hour++;
+                }
+                _minute = value;
+            }
+        }
+
+        public byte Second
+        {
+            get
+            {
+                return _second;
+            }
+            set
+            {
+                if (value > (SecondsInMinute - 1))
+                {
+                    value -= SecondsInMinute;
+                    Minute++;
+                }
+                _second = value;
+            }
+        }
+
+        #endregion
     }
 }
